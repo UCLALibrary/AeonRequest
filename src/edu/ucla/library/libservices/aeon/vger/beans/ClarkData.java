90520251714c96dfa367c19d2639261c41ef485d
David Rickard <drickard1967@library.ucla.edu>
2019-07-11 09:24:43 -0700

    WS-1540
    Clark Library updates

---
+++ src/edu/ucla/library/libservices/aeon/vger/beans/ClarkData.java
@@ -0,0 +1,131 @@
+package edu.ucla.library.libservices.aeon.vger.beans;
+
+public class ClarkData
+{
+  private String itemTitle;
+  private String itemAuthor;
+  private String itemPlace;
+  private String itemPublisher;
+  private String itemDate;
+  private String itemEdition;
+  private String callNumber;
+  private String itemVolume;
+  private String itemIssue;
+  private String itemInfo1;
+  private String location;
+
+  public ClarkData()
+  {
+    super();
+  }
+
+  public void setItemTitle( String itemTitle )
+  {
+    this.itemTitle = itemTitle;
+  }
+
+  public String getItemTitle()
+  {
+    return itemTitle;
+  }
+
+  public void setItemAuthor( String itemAuthor )
+  {
+    this.itemAuthor = itemAuthor;
+  }
+
+  public String getItemAuthor()
+  {
+    return itemAuthor;
+  }
+
+  public void setItemPlace( String itemPlace )
+  {
+    this.itemPlace = itemPlace;
+  }
+
+  public String getItemPlace()
+  {
+    return itemPlace;
+  }
+
+  public void setItemPublisher( String itemPublisher )
+  {
+    this.itemPublisher = itemPublisher;
+  }
+
+  public String getItemPublisher()
+  {
+    return itemPublisher;
+  }
+
+  public void setItemDate( String itemDate )
+  {
+    this.itemDate = itemDate;
+  }
+
+  public String getItemDate()
+  {
+    return itemDate;
+  }
+
+  public void setItemEdition( String itemEdition )
+  {
+    this.itemEdition = itemEdition;
+  }
+
+  public String getItemEdition()
+  {
+    return itemEdition;
+  }
+
+  public void setCallNumber( String callNumber )
+  {
+    this.callNumber = callNumber;
+  }
+
+  public String getCallNumber()
+  {
+    return callNumber;
+  }
+
+  public void setItemVolume( String itemVolume )
+  {
+    this.itemVolume = itemVolume;
+  }
+
+  public String getItemVolume()
+  {
+    return itemVolume;
+  }
+
+  public void setItemIssue( String itemIssue )
+  {
+    this.itemIssue = itemIssue;
+  }
+
+  public String getItemIssue()
+  {
+    return itemIssue;
+  }
+
+  public void setItemInfo1( String itemInfo1 )
+  {
+    this.itemInfo1 = itemInfo1;
+  }
+
+  public String getItemInfo1()
+  {
+    return itemInfo1;
+  }
+
+  public void setLocation( String location )
+  {
+    this.location = location;
+  }
+
+  public String getLocation()
+  {
+    return location;
+  }
+}
